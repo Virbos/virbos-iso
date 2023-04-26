1489a72784186127de54bc82a8647b6a3c734653
Jeremy <jtbx@duck.com>
2023-04-26 20:28:49 +1200

    Add user 'liveuser' and change arch linux branding to Virbos ;)

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ This repository contains source files for the Virbos live ISO.
 
 It is built using archiso, install it using `pacman -S archiso`.
 
+After that, just run `make` as root in the project's directory.
+
 In case you didn't know, here's a quick list of information
 you might need to know about archiso and how to modify how it
 builds ISOs:
--- profiledef.sh
+++ profiledef.sh
@@ -16,10 +16,11 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
-  ["/etc/shadow"]="0:0:400"
-  ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
+	["/etc/shadow"]="0:0:0400"
+	["/etc/gshadow"]="0:0:0400"
+	["/root"]="0:0:750"
+	["/root/.automated_script.sh"]="0:0:755"
+	["/usr/local/bin/choose-mirror"]="0:0:755"
+	["/usr/local/bin/Installation_guide"]="0:0:755"
+	["/usr/local/bin/livecd-sound"]="0:0:755"
 )
